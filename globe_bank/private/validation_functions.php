effcb37fbdd804525cd813b8ad1f31dd86e7f826
jacksmith89 <wreid0@gmail.com>
2022-08-16 16:29:08 -0600

    Updated through chapter 7, CRUD

---
+++ globe_bank/private/validation_functions.php
@@ -0,0 +1,6 @@
+<?php 
+// is_blank('abcd)
+// * validate data presence
+// * uses trim() so empty spaces don't count
+// * uses === to avoid false positives
+// * better than empty() which considers "0" to be empty
